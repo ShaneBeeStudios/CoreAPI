833cec13ab7cbb5808497d6d1e824630a914d3c8
shanebeee <shanebolenback@me.com>
2024-10-04 13:18:53 -0700

    McUtils - add some safety checks

--- src/main/java/com/shanebeestudios/coreapi/util/McUtils.java
+++ src/main/java/com/shanebeestudios/coreapi/util/McUtils.java
@@ -1,6 +1,7 @@
 package com.shanebeestudios.coreapi.util;
 
 
+import com.google.common.base.Preconditions;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -51,6 +52,7 @@ import java.util.stream.Collectors;
 /**
  * Utility class for changing Minecraft to/from Bukkit classes
  */
+@SuppressWarnings({"unused", "ConstantValue"})
 public class McUtils {
 
     private McUtils() {
@@ -66,17 +68,19 @@ public class McUtils {
      */
     @NotNull
     public static BlockPos getPos(@NotNull Location location) {
+        Preconditions.checkArgument(location != null, "Location cannot be null");
         return new BlockPos(location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
 
     /**
-     * Get a Minecraft Vec# from a Bukkit Location
+     * Get a Minecraft Vec3 from a Bukkit Location
      *
      * @param location Location to convert to Vec3
      * @return Vec3 from Location
      */
     @NotNull
-    public static Vec3 getVec3(Location location) {
+    public static Vec3 getVec3(@NotNull Location location) {
+        Preconditions.checkArgument(location != null, "Location cannot be null");
         return new Vec3(location.getX(), location.getY(), location.getZ());
     }
 
@@ -87,8 +91,8 @@ public class McUtils {
      * @return Minecraft Direction from BlockFace
      */
     @NotNull
-    public static Direction getDirection(BlockFace blockFace) {
-        if (blockFace == null) return Direction.UP;
+    public static Direction getDirection(@NotNull BlockFace blockFace) {
+        Preconditions.checkArgument(blockFace != null, "BlockFace cannot be null");
         return switch (blockFace) {
             case DOWN -> Direction.DOWN;
             case NORTH -> Direction.NORTH;
@@ -107,11 +111,11 @@ public class McUtils {
      */
     @NotNull
     public static Pair<ServerLevel, BlockPos> getLevelPos(@NotNull Location location) {
+        Preconditions.checkArgument(location != null, "Location cannot be null");
         BlockPos pos = getPos(location);
         World bukkitWorld = location.getWorld();
-        if (bukkitWorld == null) {
-            throw new IllegalArgumentException("Missing world in location");
-        }
+        Preconditions.checkArgument(bukkitWorld == null, "Missing world in location");
+        assert bukkitWorld != null;
         ServerLevel serverLevel = getServerLevel(bukkitWorld);
         return new Pair<>(serverLevel, pos);
     }
@@ -124,7 +128,9 @@ public class McUtils {
      * @return Location from BlockPos/Level
      */
     @NotNull
-    public static Location getLocation(BlockPos blockPos, Level level) {
+    public static Location getLocation(@NotNull BlockPos blockPos, @NotNull Level level) {
+        Preconditions.checkArgument(blockPos != null, "BlockPos cannot be null");
+        Preconditions.checkArgument(level != null, "Level cannot be null");
         return new Location(level.getWorld(), blockPos.getX(), blockPos.getY(), blockPos.getZ());
     }
 
@@ -135,7 +141,8 @@ public class McUtils {
      * @return ResourceLocation from NamespacedKey
      */
     @NotNull
-    public static ResourceLocation getResourceLocation(NamespacedKey bukkitKey) {
+    public static ResourceLocation getResourceLocation(@NotNull NamespacedKey bukkitKey) {
+        Preconditions.checkArgument(bukkitKey != null, "NamespacedKey cannot be null");
         return ResourceLocation.fromNamespaceAndPath(bukkitKey.getNamespace(), bukkitKey.getKey());
     }
 
@@ -146,7 +153,8 @@ public class McUtils {
      * @return ResourceLocation from NamespacedKey
      */
     @NotNull
-    public static NamespacedKey getNamespacedKey(ResourceLocation resourceLocation) {
+    public static NamespacedKey getNamespacedKey(@NotNull ResourceLocation resourceLocation) {
+        Preconditions.checkArgument(resourceLocation != null, "ResourceLocation cannot be null");
         return new NamespacedKey(resourceLocation.getNamespace(), resourceLocation.getPath());
     }
 
@@ -158,6 +166,7 @@ public class McUtils {
      */
     @NotNull
     public static ServerLevel getServerLevel(@NotNull World world) {
+        Preconditions.checkArgument(world != null, "World cannot be null");
         return ((CraftWorld) world).getHandle();
     }
 
@@ -180,7 +189,8 @@ public class McUtils {
      * @param chunk Bukkit Chunk to convert
      * @return LevelChunk from Chunk
      */
-    public static LevelChunk getLevelChunk(Chunk chunk) {
+    public static LevelChunk getLevelChunk(@NotNull Chunk chunk) {
+        Preconditions.checkArgument(chunk != null, "Chunk cannot be null");
         ServerLevel serverLevel = getServerLevel(chunk.getWorld());
         return serverLevel.getChunk(chunk.getX(), chunk.getZ());
     }
@@ -193,7 +203,8 @@ public class McUtils {
      * @param <T>         ResourceKey
      * @return Registry from key
      */
-    public static <T> Registry<T> getRegistry(ResourceKey<? extends Registry<? extends T>> registryKey, boolean unfreeze) {
+    public static <T> Registry<T> getRegistry(@NotNull ResourceKey<? extends Registry<? extends T>> registryKey, boolean unfreeze) {
+        Preconditions.checkArgument(registryKey != null, "ResourceKey cannot be null");
         Registry<T> registry = MinecraftServer.getServer().registryAccess().registryOrThrow(registryKey);
         if (unfreeze) {
             ReflectionUtils.setField("frozen", registry, false);
@@ -210,6 +221,7 @@ public class McUtils {
      */
     @NotNull
     public static ServerPlayer getServerPlayer(@NotNull Player player) {
+        Preconditions.checkArgument(player != null, "Player cannot be null");
         return ((CraftPlayer) player).getHandle();
     }
 
@@ -219,7 +231,8 @@ public class McUtils {
      * @param bukkitEntity Bukkit Entity
      * @return NMS Entity
      */
-    public static Entity getNMSEntity(org.bukkit.entity.Entity bukkitEntity) {
+    public static Entity getNMSEntity(@NotNull org.bukkit.entity.Entity bukkitEntity) {
+        Preconditions.checkArgument(bukkitEntity != null, "Entity cannot be null");
         return ((CraftEntity) bukkitEntity).getHandle();
     }
 
@@ -229,7 +242,8 @@ public class McUtils {
      * @param bukkitType Bukkit EntityType to convert
      * @return NMS EntityType from Bukkit
      */
-    public static EntityType<?> getEntityType(org.bukkit.entity.EntityType bukkitType) {
+    public static EntityType<?> getEntityType(@NotNull org.bukkit.entity.EntityType bukkitType) {
+        Preconditions.checkArgument(bukkitType != null, "EntityType cannot be null");
         NamespacedKey key = bukkitType.getKey();
         ResourceLocation resourceLocation = McUtils.getResourceLocation(key);
         return BuiltInRegistries.ENTITY_TYPE.get(resourceLocation);
@@ -242,7 +256,8 @@ public class McUtils {
      * @return BlockData from state
      */
     @NotNull
-    public static BlockData getBlockDataFromState(BlockState blockState) {
+    public static BlockData getBlockDataFromState(@NotNull BlockState blockState) {
+        Preconditions.checkArgument(blockState != null, "BlockState cannot be null");
         BlockData blockDataFromBlockState = CraftBlockData.fromData(blockState);
         return blockDataFromBlockState != null ? blockDataFromBlockState : AIR;
     }
@@ -254,7 +269,8 @@ public class McUtils {
      * @return BlockState from Bukkit Block
      */
     @NotNull
-    public static BlockState getBlockStateFromBlock(Block bukkitBlock) {
+    public static BlockState getBlockStateFromBlock(@NotNull Block bukkitBlock) {
+        Preconditions.checkArgument(bukkitBlock != null, "Block cannot be null");
         return ((CraftBlock) bukkitBlock).getNMS();
     }
 
@@ -265,7 +281,8 @@ public class McUtils {
      * @return Converted BlockState
      */
     @NotNull
-    public static BlockState getBlockStateFromData(BlockData blockData) {
+    public static BlockState getBlockStateFromData(@NotNull BlockData blockData) {
+        Preconditions.checkArgument(blockData != null, "BlockData cannot be null");
         return ((CraftBlockData) blockData).getState();
     }
 
@@ -278,7 +295,9 @@ public class McUtils {
      * @return Holder from registry
      */
     @Nullable
-    public static <T> Holder.Reference<T> getHolderReference(Registry<T> registry, NamespacedKey key) {
+    public static <T> Holder.Reference<T> getHolderReference(@NotNull Registry<T> registry, @NotNull NamespacedKey key) {
+        Preconditions.checkArgument(registry != null, "Registry cannot be null");
+        Preconditions.checkArgument(key != null, "NamespacedKey cannot be null");
         ResourceLocation resourceLocation = McUtils.getResourceLocation(key);
         ResourceKey<T> resourceKey = ResourceKey.create(registry.key(), resourceLocation);
         try {
@@ -297,7 +316,9 @@ public class McUtils {
      * @return Value from registry
      */
     @Nullable
-    public static <T> T getRegistryValue(Registry<T> registry, NamespacedKey key) {
+    public static <T> T getRegistryValue(@NotNull Registry<T> registry, @NotNull NamespacedKey key) {
+        Preconditions.checkArgument(registry != null, "Registry cannot be null");
+        Preconditions.checkArgument(key != null, "NamespacedKey cannot be null");
         Holder.Reference<T> holderReference = getHolderReference(registry, key);
         return holderReference != null ? holderReference.value() : null;
     }
@@ -310,7 +331,8 @@ public class McUtils {
      * @return List of NamespacedKeys for all keys in registry
      */
     @NotNull
-    public static <T> List<NamespacedKey> getRegistryKeys(Registry<T> registry) {
+    public static <T> List<NamespacedKey> getRegistryKeys(@NotNull Registry<T> registry) {
+        Preconditions.checkArgument(registry != null, "Registry cannot be null");
         List<NamespacedKey> keys = new ArrayList<>();
         registry.keySet().forEach(resourceLocation -> {
             NamespacedKey namespacedKey = McUtils.getNamespacedKey(resourceLocation);
@@ -330,7 +352,7 @@ public class McUtils {
      * @return Biome resolver
      */
     @NotNull
-    public static BiomeResolver getBiomeResolver(MutableInt count, ChunkAccess chunkAccess, BoundingBox box, Holder<Biome> biome, Predicate<Holder<Biome>> filter) {
+    public static BiomeResolver getBiomeResolver(@NotNull MutableInt count, @NotNull ChunkAccess chunkAccess, @NotNull BoundingBox box, @NotNull Holder<Biome> biome, @NotNull Predicate<Holder<Biome>> filter) {
         return (x, y, z, noise) -> {
             Holder<Biome> biomeHolder = chunkAccess.getNoiseBiome(x, y, z);
             if (box.isInside(x << 2, y << 2, z << 2) && filter.test(biomeHolder)) {
@@ -351,7 +373,8 @@ public class McUtils {
      * @param z          z coord of block in chunk
      * @return Biome at position in chunk
      */
-    public static Biome getBiomeInChunk(LevelChunk levelChunk, int x, int y, int z) {
+    public static Biome getBiomeInChunk(@NotNull LevelChunk levelChunk, int x, int y, int z) {
+        Preconditions.checkArgument(levelChunk != null, "LevelChunk cannot be null");
         int chunkX = levelChunk.getPos().x;
         int chunkZ = levelChunk.getPos().z;
         return levelChunk.getNoiseBiome((x + (chunkX << 4)) >> 2, y >> 2, (z + (chunkZ << 4)) >> 2).value();
@@ -367,7 +390,8 @@ public class McUtils {
      * @param z     z coord of block in chunk
      * @return Biome at position in chunk
      */
-    public static Biome getBiomeInChunk(Chunk chunk, int x, int y, int z) {
+    public static Biome getBiomeInChunk(@NotNull Chunk chunk, int x, int y, int z) {
+        Preconditions.checkArgument(chunk != null, "Chunk cannot be null");
         LevelChunk levelChunk = getLevelChunk(chunk);
         return getBiomeInChunk(levelChunk, x, y, z);
     }
