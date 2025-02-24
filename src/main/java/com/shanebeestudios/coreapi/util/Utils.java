f6c13baa025d9f3504d13cfd09ee1aa016edf01d
ShaneBeee <shanebolenback@me.com>
2025-02-24 15:22:36 -0800

    Utils - switch to mini message

--- src/main/java/com/shanebeestudios/coreapi/util/Utils.java
+++ src/main/java/com/shanebeestudios/coreapi/util/Utils.java
@@ -20,6 +20,7 @@ public class Utils {
 
     private static final CommandSender CONSOLE = Bukkit.getConsoleSender();
     private static String PREFIX = "&7[&bCoreApi&7] ";
+    private static String MINI_PREFIX = "<grey>[<aqua>CoreApi<grey>] ";
     private static final Pattern HEX_PATTERN = Pattern.compile("<#([A-Fa-f\\d]){6}>");
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
@@ -27,9 +28,20 @@ public class Utils {
      * Set the prefix to be used in messages
      *
      * @param prefix Prefix to be used in messages
+     * @deprecated Use {@link #setMiniPrefix(String)} instead
      */
+    @Deprecated
     public static void setPrefix(String prefix) {
         PREFIX = prefix;
+    }
+
+    /**
+     * Set the prefix to be used in mini messages
+     *
+     * @param prefix Prefix to be used in mini messages
+     */
+    public static void setMiniPrefix(String prefix) {
+        MINI_PREFIX = prefix;
     }
 
     /**
@@ -37,8 +49,9 @@ public class Utils {
      *
      * @param string String to apply color to
      * @return Colored string
+     * @deprecated Use {@link #getMini(String)} instead
      */
-    @SuppressWarnings("deprecation") // Paper deprecation
+    @Deprecated
     public static String getColString(String string) {
         Matcher matcher = HEX_PATTERN.matcher(string);
         while (matcher.find()) {
@@ -67,9 +80,21 @@ public class Utils {
      *
      * @param message Formatted message
      * @param objects Objects for format
+     * @deprecated Use {@link #logMini(String, Object...)} instead
      */
+    @Deprecated
     public static void log(String message, Object... objects) {
         sendTo(CONSOLE, message, objects);
+    }
+
+    /**
+     * Log a formatted mini-message to console
+     *
+     * @param message Formatted message
+     * @param objects Objects for format
+     */
+    public static void logMini(String message, Object... objects) {
+        sendMiniTo(CONSOLE, message, objects);
     }
 
     /**
@@ -78,7 +103,9 @@ public class Utils {
      * @param sender  Sender to receive message
      * @param message Formatted message to send
      * @param objects Objects to include in format
+     * @deprecated Use {@link #sendMiniTo(CommandSender, String, Object...)} instead
      */
+    @Deprecated
     public static void sendTo(CommandSender sender, String message, Object... objects) {
         String format;
         if (objects.length > 0) {
@@ -90,12 +117,30 @@ public class Utils {
     }
 
     /**
+     * Send a colored/prefixed mini-message to a command sender
+     *
+     * @param sender  Sender to receive message
+     * @param message Formatted message to send
+     * @param objects Objects to include in format
+     */
+    public static void sendMiniTo(CommandSender sender, String message, Object... objects) {
+        String format;
+        if (objects.length > 0) {
+            format = String.format(message, objects);
+        } else {
+            format = message;
+        }
+        sender.sendMessage(getMini(MINI_PREFIX + format));
+    }
+
+    /**
      * Broadcast a message to all players and console
      *
      * @param message Formatted message to send
      * @param objects Objects to include in format
+     * @deprecated Use {@link #broadcastMini(String, Object...)} instead
      */
-    @SuppressWarnings("deprecation")
+    @Deprecated
     public static void broadcast(String message, Object... objects) {
         String format;
         if (objects.length > 0) {
@@ -104,6 +149,22 @@ public class Utils {
             format = message;
         }
         Bukkit.broadcastMessage(getColString(PREFIX + format));
+    }
+
+    /**
+     * Broadcast a mini-message to all players and console
+     *
+     * @param message Formatted message to send
+     * @param objects Objects to include in format
+     */
+    public static void broadcastMini(String message, Object... objects) {
+        String format;
+        if (objects.length > 0) {
+            format = String.format(message, objects);
+        } else {
+            format = message;
+        }
+        Bukkit.broadcast(getMini(MINI_PREFIX + format));
     }
 
 }
